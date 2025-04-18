7cfb56df9d645fb02809b6c393fa34adae19812d
kevinmel12 <melkevinmeledje@gmail.com>
2025-04-18 14:20:25 +0200

    first step

--- figma-plugin/webpack.config.js
+++ figma-plugin/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = (env, argv) => {
       new CopyPlugin({
         patterns: [
           {
-            from: './src/ui/ProjectOverviewPage.html',
+            from: './src/ui/AnalysisCardPage.html',
             to: './ui.html',
           },
           {
@@ -52,7 +52,7 @@ module.exports = (env, argv) => {
             to: './',
           },
           {
-            from: './src/ui/ProjectOverviewPage.html',
+            from: './src/ui/AnalysisCardPage.html',
             to: './ui/',
           }
         ],
